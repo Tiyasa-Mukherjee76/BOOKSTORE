313570993456516869dd46ef9967ea3420c18011
Tiyasa-Mukherjee76 <2205425@kiit.ac.in>
2025-04-09 20:56:26 +0530

    Delete Book.js

--- Book.js
+++
@@ -1,9 +0,0 @@
-const bSchema = new mg2.Schema({
-    title: String,
-    author: String,
-    price: Number,
-    description: String,
-  });
-  
-  module.exports = mg2.model("Book", bSchema);
-  
